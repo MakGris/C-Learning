6a832c7a9c225e585dc99fca50e6054dc6ec0d68
MakGris <maxgrishenko@gmail.com>
2023-02-09 17:54:55 +0700

    add charvars commit

---
+++ charVars/charVars/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  charVars
+//
+//  Created by Maksim Grischenko on 09.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
