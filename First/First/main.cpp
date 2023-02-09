d0a0337aa6dd710f4886a38256a27f4b2209deed
MakGris <maxgrishenko@gmail.com>
2023-02-09 17:37:19 +0700

    initial commit

---
+++ First/First/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  First
+//
+//  Created by Maksim Grischenko on 07.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    // insert code here...
+    cout << "У каждой эпохи свой язык\n";
+    return 0;
+}
---
+++ intvars/intvars/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  intvars
+//
+//  Created by Maksim Grischenko on 09.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int var1;
+    int var2;
+    var1 = 20;
+    var2 = var1 + 10;
+    cout << "var1 + 10 равно ";
+    cout << var2 << endl;
+    return 0;
+}
