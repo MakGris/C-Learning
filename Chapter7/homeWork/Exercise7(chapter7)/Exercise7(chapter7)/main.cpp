0780655a528780214f03a6feff2a33c367eab5b7
MakGris <maxgrishenko@gmail.com>
2023-07-21 19:51:02 +0700

    try exercise 7(chapter7)

---
+++ Chapter7/homeWork/Exercise7(chapter7)/Exercise7(chapter7)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Exercise7(chapter7)
+//
+//  Created by Maksim Grischenko on 21.07.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
