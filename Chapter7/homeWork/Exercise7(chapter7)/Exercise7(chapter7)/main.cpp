d1b3bc502de2c7399f6ffb816ae3e398ed024e42
MakGris <maxgrishenko@gmail.com>
2023-07-27 19:29:58 +0700

    complete exercise 7(chapter7)

--- Chapter7/homeWork/Exercise7(chapter7)/Exercise7(chapter7)/main.cpp
+++ Chapter7/homeWork/Exercise7(chapter7)/Exercise7(chapter7)/main.cpp
@@ -9,23 +9,37 @@
 #include <math.h>
 #include <iomanip>
 using namespace std;
+double convertCharToDouble (char[]);
 int main() {
-    const int max = 21;
-    char amount [21] = "1,234,567,890,123.99";
-    char amount2 [max];
+    char amount [22];
+    char repeat;
+    do {
+        cout << "Введите денежную сумму(в виде $1,000,000.000):";
+        cin >> amount;
+        cout << setprecision(15) << "Сумма после конвертации в double: "
+        << convertCharToDouble(amount) << endl;
+        cout << "Повторить ввод?(y/n)?:";
+        cin >> repeat;
+        
+        
+    }while(repeat != 'n');
     
+    
+    
+
+    return 0;
+}
+double convertCharToDouble(char amount[]) {
+    const int max = 22;
+    char amount2 [max];
     for(int i = 0, j = 0; i < max; i++) {
-        if(amount[i] != ',') {
+        if(amount[i] != ',' && amount[i] != '$' ) {
             amount2[j++] = amount[i];
         } else {
             amount2[j] = amount[i];
             }
         }
     double money = atof(amount2);
-    cout << setprecision(15) << money << endl;
-    
-    
-
-    return 0;
+    return money;
 }
 
