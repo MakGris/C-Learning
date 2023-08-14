9d69f500c522063fb5f5b2e7ddfb14022719e917
MakGris <maxgrishenko@gmail.com>
2023-08-14 18:53:43 +0700

    continue exercise11(chapter7)

--- Chapter7/homeWork/exercise11(chapter7)/exercise11(chapter7)/main.cpp
+++ Chapter7/homeWork/exercise11(chapter7)/exercise11(chapter7)/main.cpp
@@ -9,26 +9,27 @@
 #include <math.h>
 #include <iomanip>
 #include <string>
+#include <cstring>
 #include <sstream>
 using namespace std;
 double convertCharToDouble (char[]);
 void convertDoubleToChar(double);
 int main() {
     
-//    char amount [22];
-//    char repeat;
-//    do {
-//        cout << "Введите денежную сумму(в виде $1,000,000.000):";
-//        cin >> amount;
-//        cout << setprecision(15) << "Сумма после конвертации в double: "
-//        << convertCharToDouble(amount) << endl;
-//        cout << "Повторить ввод?(y/n)?:";
-//        cin >> repeat;
-//
-//
-//    }while(repeat != 'n');
+    //    char amount [22];
+    //    char repeat;
+    //    do {
+    //        cout << "Введите денежную сумму(в виде $1,000,000.000):";
+    //        cin >> amount;
+    //        cout << setprecision(15) << "Сумма после конвертации в double: "
+    //        << convertCharToDouble(amount) << endl;
+    //        cout << "Повторить ввод?(y/n)?:";
+    //        cin >> repeat;
+    //
+    //
+    //    }while(repeat != 'n');
     
-    double sum = 1234567891.99;
+    long double sum = 1234.99;
     convertDoubleToChar(sum);
     return 0;
 }
@@ -41,26 +42,44 @@ double convertCharToDouble(char amount[]) {
             amount2[j++] = amount[i];
         } else {
             amount2[j] = amount[i];
-            }
         }
+    }
     double money = atof(amount2);
     return money;
 }
 
 void convertDoubleToChar(double amount) {
-    stringstream strs;
-    strs << setprecision(16) << amount;
+    ostringstream strs;
+//    amount = floor(amount * 100)/100;
+    strs << setprecision(18) << amount;
     string str = strs.str();
-    char charArray[22] = "$";
-    for(int i = 0, j = 1; i < 22; i++) {
+    char charArray[26] = "$0,000,000,000,000,000.00";
+    
+    
+    for (int i = 0, j = 1 ; i < str.size(); i++) {
         charArray[j++] = str[i];
-        }
-    
-    cout << 4%2 << endl;
-    cout << charArray;
+//        if(i==3) {
+//            charArray[j++] = ',';
+//            charArray[j++] = str[i];
+//
+//        } else if(i==6) {
+//            charArray[j++] = ',';
+//            charArray[j++] = str[i];
+//        }else if (i==9) {
+//            charArray[j++] = ',';
+//            charArray[j++] = str[i];
+//        }
+//        else {
+//            charArray[j++] = str[i];
+//
+//        }
+
+        
     }
-    
-    
-    
+    cout << charArray << endl;
+}
+
+
+
 
 
